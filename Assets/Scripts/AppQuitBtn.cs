71eab5d1170b6c02c032b065bc0abb3900e782ea
Apotheous <brucoglu@outlook.com>
2025-09-07 20:22:37 +0300

    some-names-and-appquitbtn

---
+++ Assets/Scripts/AppQuitBtn.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppQuitBtn : MonoBehaviour
+{
+    public void AppQit()
+    {
+        Application.Quit();
+    }
+}
